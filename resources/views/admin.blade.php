6d25da3ef39e4fefe8a8be7a1a6588290e1e56b1
Kamil <kamilczekotas@gmail.com>
2023-08-21 20:38:29 +0200

    Removing data from adminPanel for normal users

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -11,22 +11,26 @@
 @show
 
 @section('content')
+
+@if((Auth::user()->id) == 1)
 @foreach($users as $user)
-    <div class="dataBlock aPanel">
+<div class="dataBlock aPanel">
+    <div>
         <div>
-            <div>
-                {{ $user->company }}
-            </div>
-        </div>
-        <div>
-            {{ $user->email }}
-        </div>
-        <div class="last">
-            <a href="/deleteuser/{{ $user->id }}" class="btn remove">Remove</a>
+            {{ $user->company }}
         </div>
     </div>
+    <div>
+        {{ $user->email }}
+    </div>
+    <div class="last">
+        <a href="/deleteuser/{{ $user->id }}" class="btn remove">Remove</a>
+    </div>
+</div>
 @endforeach
 </div>
+@endif
+
 
 
 @stop
